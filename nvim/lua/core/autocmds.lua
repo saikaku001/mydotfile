cd025f43c6e17aabd78990e7c34e9e3d521e2dac
Saikaku Lab <130112558+saikaku001@users.noreply.github.com>
2026-03-30 13:25:16 +0900

    Update autocmds.lua

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -1,3 +1,17 @@
+vim.api.nvim_create_autocmd("ColorScheme", {
+  group = vim.api.nvim_create_augroup("TransparentBackground", { clear = true }),
+  callback = function()
+    vim.cmd([[
+      highlight Normal guibg=none ctermbg=none
+      highlight NonText guibg=none ctermbg=none
+      highlight NormalNC guibg=none ctermbg=none
+      highlight SignColumn guibg=none ctermbg=none
+      highlight NormalFloat guibg=none ctermbg=none
+      highlight FloatBorder guibg=none ctermbg=none
+    ]])
+  end,
+})
+
 vim.api.nvim_create_autocmd("VimEnter", {
   group = vim.api.nvim_create_augroup("StartupTime", { clear = true }),
   callback = function()
